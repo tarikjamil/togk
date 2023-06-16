fb82c28b487e893ade39c0b949a21e7a7bc8eb88
tarikjamil <tarikjamill@gmail.com>
2023-06-16 10:19:13 +0100

    Update kalendar.js

--- kalendar.js
+++ kalendar.js
@@ -1,31 +1,33 @@
 const calendarItems = document.querySelector(".kalendar-items");
-const items = calendarItems.querySelectorAll(".kalendar-item");
+const items = Array.from(calendarItems.querySelectorAll(".kalendar-item"));
 
 const months = {};
 
-// Group dates by month
+// Detach items and group them by month
 items.forEach((item) => {
   const monthName = item.querySelector('[trigger="month"]').textContent;
   if (!months[monthName]) {
     months[monthName] = [];
   }
+  // Detach the item from the DOM
+  item.parentNode.removeChild(item);
+  // Push the item into the month array
   months[monthName].push(item);
 });
 
-// Add h2 elements for each month
+// Add h2 elements for each month and insert the items in the correct order
 for (const monthName in months) {
   const monthItems = months[monthName];
+  
+  // Create and insert the month header
   const monthHeader = document.createElement("h2");
   monthHeader.setAttribute("animation", "loading");
   monthHeader.setAttribute("class", "heading--48 is--kalendar");
   monthHeader.textContent = monthName;
-  calendarItems.insertBefore(monthHeader, monthItems[0]);
-
-  // keep a reference to the node before which you want to insert items
-  let referenceNode = monthHeader.nextSibling;
-
-  // insert each item before the reference node
+  calendarItems.appendChild(monthHeader);
+  
+  // Insert the items for the current month after the header
   monthItems.forEach((item) => {
-    calendarItems.insertBefore(item, referenceNode);
+    calendarItems.appendChild(item);
   });
 }
